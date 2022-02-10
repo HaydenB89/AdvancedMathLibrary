989db35f3ce6609b491645d24749403f88006269
hayden brown <haydenbrowncinci@gmail.com>
2022-02-10 10:26:37 -0500

    added constant Zero and tested

--- AdvancedMathLibrary/Math.cs
+++ AdvancedMathLibrary/Math.cs
@@ -3,6 +3,7 @@
 namespace AdvancedMathLibrary {
 
     public static class Math {
+        public const int Zero = 0;
 
         public static int AbsoluteValue(int a) { //abs vals only have one parameter
             return (a < 0) ? -a : a;
--- TestAdvMathLibrary/TestIntMathLib.cs
+++ TestAdvMathLibrary/TestIntMathLib.cs
@@ -9,6 +9,13 @@ namespace TestAdvMathLibrary {
         public void TestAbsoluteValue() {   //test methods ALWAYS return void w/no parameters
             int answer = Math.AbsoluteValue(-3);
             Assert.Equal(3, answer);
+            //testing with 3
+            Assert.Equal(3, Math.AbsoluteValue(3));
+            Assert.Equal(0, Math.AbsoluteValue(0));
+        }
+        [Fact]
+        public void TestZeroConstant() {
+            Assert.Equal(0, Math.Zero);
         }
     }
 }
